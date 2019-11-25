5ac12cdefb3e192c19cfd3756458f6dbed8ef50f
DeniseCastro <58062999+DeniseCastro@users.noreply.github.com>
2019-11-24 21:36:26 -0700

    Update Savings.java

--- lab4/Savings.java
+++ lab4/Savings.java
@@ -1,76 +1,81 @@
-// Extends the features of account
-public class Savings extends Account{
+public class Credit extends Account{
+  private int account_credit_max;
 
     /**
+     * Constructor
      * @param accNum
      * @param b
      * @return
      */
-    // contsructor
-    Savings(int accNum, double b){
-        // calls the Account Constructor
+    Credit(int accNum, double b, int creditMax){
         super(accNum, b);
+        this.account_credit_max = creditMax;
     }
 
+    /**
+     * getMax: gets credit limit
+     * @return double: credit max
+     */
+    public double getMax(){
+      return this.account_credit_max;
+    }
 
     /**
-     * withdraw: This Method overides the funtionality of withdraw
-     * method in account class. User will not be able to withdraw
-     * money from their savings account
+     * deposit: In credit account, the user is not able to make a deposit
+     * @param amount double: how much to deposit
+     * @return boolean: successful
+     */
+    @Override
+    public boolean deposit(double amount){
+        System.out.println("Cannot deposit to Credit Account");
+        return false;
+    }
+
+    /**
+     * withdraw: In credit account, the user is not able to make a withdraw
      * @param amount double: how much to withdraw
-     * @return boolean: succesful
+     * @return boolean: successful
      */
     @Override
     public boolean withdraw(double amount){
-      // if balance of checking is less than or equal than 0
-      if(super.getBalance() <= 0){
-          System.out.println("\n********************");
-          System.out.println("Please Deposit");
-          System.out.println("********************\n");
-          return false;
-      }
-      //amount is less than or greater than the current balance the user has
-      if(amount < 0 || amount >= super.getBalance()){
-          System.out.println("\n********************");
-          System.out.println("Please enter a correct amount");
-          System.out.println("********************\n");
-          return false;
-      }
-      // updates the balance
-      super.updateBalance(super.getBalance()-amount);
-      return true;
+        System.out.println("Cannot withdraw from Credit Account");
+        return false;
     }
 
     /**
-     * getMax: gets max. only works in credit account
-     * @return double: feature not avaiable
+     * transfer: The user will be able to transfer money into the credit account
+     * to pay their debt.
+     * @param checkingAccount Account: user checking account
+     * @param amount double: amount to transfer
+     * @return boolean: successful
      */
     @Override
-    public double getMax(){
-      return -1;
-    }
-
-
-    /**
-     * transfer: This method will override the transfer method in account class.
-     * Will transfer money to the transfer account the user selected
-     *
-     * @param transferAccount Account: other user account
-     * @param amount Double: how much to transfer
-     * @return boolean: succesful
-     */
-    @Override
-    public boolean transfer(Account transferAccount, double amount){
-        // checks if the amount is lesss than 0 or greater than the balance the user currently has
-        if(amount < 0 || amount > super.getBalance()){
+    public boolean transfer(Account checkingAccount, double amount){
+        // amount is less than 0
+        if(amount < 0){
             System.out.println("\n********************");
             System.out.println("Please enter a correct amount");
             System.out.println("********************\n");
             return false;
+        } else
+        // amount is greater than current balance. User should only pay dept, not more
+        if(amount > -super.getBalance()){
+            System.out.println("\n********************");
+            System.out.println("Do not over pay!");
+            System.out.println("Current Balance: " + super.getBalance());
+            System.out.println("********************\n");
+            return false;
         }
-        // makes update
-        super.updateBalance(super.getBalance() - amount);
-        transferAccount.updateBalance(transferAccount.getBalance() + amount);
+        // checks if checking account has enough money
+        if(checkingAccount.getBalance() < amount){
+          System.out.println("\n********************");
+          System.out.println("Amount is greater than Checking Balance!");
+          System.out.println("********************\n");
+          return false;
+        }
+        // updates balance
+        super.updateBalance(super.getBalance() + amount);
+        checkingAccount.updateBalance(checkingAccount.getBalance() - amount);
         return true;
     }
 }
